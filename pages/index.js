6cbe2f4646351242bac2d97399182dd17f8ae821
FelipeBMN <57158909+FelipeBMN@users.noreply.github.com>
2021-03-28 13:18:37 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import { useState } from 'react';
 function Home() {
     return (
         <div>
-            <h1>Contador 10</h1>
-            <Contador />
+            <h1>Contador  <Contador /></h1>
         </div>
     ) 
 }
